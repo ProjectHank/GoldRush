d046ca26c527778f97bc297db03c563fce5567ae
ProjectHank <matt-2314@hotmail.com>
2014-06-05 19:07:43 -0300

    Intial Code commit attempt 18

---
+++ forge src/src/test/java/com/example/exampleMod/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.example.exampleMod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
