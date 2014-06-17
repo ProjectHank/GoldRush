6be366fe12b58ffb95849b5e8627931eae920977
ProjectHank <matt-2314@hotmail.com>
2014-06-17 17:54:07 -0400

    Andre - Adding unit test framework for a prices check

---
+++ forge src/src/test/java/com/example/exampleMod/materialsPriceCheck.java
@@ -0,0 +1,33 @@
+//Unit test for materials price, psuedocode until items are created
+package com.example.exampleMod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class materialsPriceCheck {
+
+	@Test
+	public void test() {
+		 org.junit.Assert.assertTrue("Gold fails",x.getPriceBuy(gold)>x.getPriceSell(gold));
+		 org.junit.Assert.assertTrue("Dirt fails",x.getPriceBuy(dirt)>x.getPriceSell(dirt));
+		 org.junit.Assert.assertTrue("Stone fails",x.getPriceBuy(stone)>x.getPriceSell(stone));
+		 org.junit.Assert.assertTrue("Wood fails",x.getPriceBuy(wood)>x.getPriceSell(wood));
+		 org.junit.Assert.assertTrue("Gold fails",x.getPriceBuy(gravel)>x.getPriceSell(gravel));
+		 org.junit.Assert.assertTrue("Gold fails",x.getPriceBuy(iron)>x.getPriceSell(iron));
+		 org.junit.Assert.assertTrue("Gold fails",x.getPriceBuy(coal)>x.getPriceSell(coal));
+		 org.junit.Assert.assertTrue("Iron Pick fails",x.getPriceBuy(ironPick)>x.getPriceSell(ironPick));
+		 org.junit.Assert.assertTrue("Stone Pick fails",x.getPriceBuy(stonePick)>x.getPriceSell(stonePick));
+		 org.junit.Assert.assertTrue("Diamond Pick fails",x.getPriceBuy(diamondPick)>x.getPriceSell(diamondPick));
+		 org.junit.Assert.assertTrue("Wooden Pick fails",x.getPriceBuy(woodpick)>x.getPriceSell(woodpick));
+		 org.junit.Assert.assertTrue("Iron axe fails",x.getPriceBuy(ironAxe)>x.getPriceSell(ironAxe));
+		 org.junit.Assert.assertTrue("Stone axe fails",x.getPriceBuy(stoneAxe)>x.getPriceSell(stoneAxe));
+		 org.junit.Assert.assertTrue("Diamond axe fails",x.getPriceBuy(diamondAxe)>x.getPriceSell(diamondAxe));
+		 org.junit.Assert.assertTrue("Wooden Pick fails",x.getPriceBuy(woodpick)>x.getPriceSell(woodpick));
+		 org.junit.Assert.assertTrue("Potato fails ",x.getPriceBuy(potato)>x.getPriceSell(potato));
+		 org.junit.Assert.assertTrue("Steak fails",x.getPriceBuy(steak)>x.getPriceSell(steak));
+		 org.junit.Assert.assertTrue("Bread fails",x.getPriceBuy(bread)>x.getPriceSell(bread));
+		 
+	}
+
+}
