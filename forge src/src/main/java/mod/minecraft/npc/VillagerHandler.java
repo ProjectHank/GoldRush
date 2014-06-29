e21ccff6d3ee544291d2f10e37161408b024e5b9
ProjectHank <matt-2314@hotmail.com>
2014-06-29 15:44:18 -0400

    Tyler M: Adding more trades recipes on VillagerHandler

--- forge src/src/main/java/mod/minecraft/npc/VillagerHandler.java
+++ forge src/src/main/java/mod/minecraft/npc/VillagerHandler.java
@@ -1,5 +1,6 @@
 package mod.minecraft.npc;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.entity.passive.EntityVillager;
@@ -28,7 +29,31 @@ public class VillagerHandler implements IVillageTradeHandler{
 	
 	private void manipulateTraderTrades(MerchantRecipeList recipeList)
 	{
-			recipeList.add(new MerchantRecipe(new ItemStack(Items.gold_nugget, 5), new ItemStack(Items.iron_pickaxe)));
-			recipeList.add(new MerchantRecipe(new ItemStack(Blocks.gold_block), new ItemStack(Items.diamond_pickaxe)));
+		ArrayList<MerchantRecipe> a = new ArrayList<MerchantRecipe>();
+		a.add(new MerchantRecipe(
+				new ItemStack(Items.gold_nugget, 64),
+				new ItemStack(Items.iron_pickaxe)));
+		a.add(new MerchantRecipe(
+				new ItemStack(Items.gold_nugget, 47),
+				new ItemStack(Items.iron_axe)));
+		a.add(new MerchantRecipe(
+				new ItemStack(Items.gold_nugget, 25),
+				new ItemStack(Items.cooked_beef)));
+		a.add(new MerchantRecipe(
+				new ItemStack(Blocks.gold_block, 1),
+				new ItemStack(Items.gold_nugget, 64)));
+		a.add(new MerchantRecipe(
+				new ItemStack(Blocks.stone, 1),
+				new ItemStack(Items.gold_nugget, 5)));
+		recipeList.addAll(a);
+		/*recipeList.addToListWithCheck(new MerchantRecipe(
+				new ItemStack(Items.gold_nugget, 5),
+				new ItemStack(Items.iron_pickaxe)));
+		recipeList.addToListWithCheck(new MerchantRecipe(
+				new ItemStack(Blocks.gold_block, 10),
+				new ItemStack(Items.diamond_pickaxe)));
+		recipeList.addToListWithCheck(new MerchantRecipe(
+				new ItemStack(Blocks.gold_block, 10),
+				new ItemStack(Items.wooden_pickaxe)));*/
 	}
 }
