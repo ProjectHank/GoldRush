0b4827bfd0bccf7cd2657e4a7ed868d2d972e61d
ProjectHank <matt-2314@hotmail.com>
2014-07-24 17:05:38 -0400

    Matt Nguyen: Updated files of FillBucket, itemBucketGoldFinder, items

--- forge src/src/main/java/mod/minecraft/item/FillBucket.java
+++ forge src/src/main/java/mod/minecraft/item/FillBucket.java
@@ -1,24 +1,51 @@
 package mod.minecraft.item;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import cpw.mods.fml.common.eventhandler.Event.Result;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
-public class FillBucket extends PlayerEvent {
-	public final ItemStack current;
-    public final World world;
-    public final MovingObjectPosition target;
+public class FillBucket {
+	public static FillBucket INSTANCE = new FillBucket();
+	public Map<Block, Item> buckets = new HashMap<Block, Item>();
 
     public ItemStack result;
 
-    public FillBucket(EntityPlayer player, ItemStack current, World world, MovingObjectPosition target)
-    {
-        super(player);
-        this.current = current;
-        this.world = world;
-        this.target = target;
+    private FillBucket(){
     }
-
+    
+    @SubscribeEvent
+    public void onBucketFill(FillBucketEvent event){
+    	
+    	ItemStack result = fillCustomBucket(event.world, event.target);
+    	
+    	if(result == null){
+    		return;
+    	}
+    	
+    	event.result = result;
+    	event.setResult(Result.ALLOW);
+    }
+    
+    private ItemStack fillCustomBucket(World world, MovingObjectPosition pos){
+    	Block block = world.getBlock(pos.blockX, pos.blockY, pos.blockZ);
+    	
+    	Item bucket = buckets.get(block);
+    	
+    	if(block != Blocks.water){
+    		return null;
+    	}
+    	else
+    		return new ItemStack(bucket);
+    }
 }
--- forge src/src/main/java/mod/minecraft/item/itemBucketGoldFinder.java
+++ forge src/src/main/java/mod/minecraft/item/itemBucketGoldFinder.java
@@ -2,7 +2,9 @@ package mod.minecraft.item;
 
 import javax.swing.Icon;
 
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import mod.minecraft.block.ExampleMod;
@@ -17,6 +19,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -24,21 +27,20 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class itemBucketGoldFinder extends ItemBucket{
 	
 	private Block isFilled;
 	private static final String __OBFID = "CL_00000000";
 	
-	private static final ResourceLocation texture = new ResourceLocation(null, null); 
+	private static final ResourceLocation texture = new ResourceLocation(items.MODID, "item/bucketgoldfinder.png"); 
 
 	public itemBucketGoldFinder(Block par1){
 		
 		super(par1);
-		
 		this.maxStackSize = 1;
 		this.isFilled = par1;
 		this.setCreativeTab(CreativeTabs.tabTools);
-	}
-	
+	}	
 }
